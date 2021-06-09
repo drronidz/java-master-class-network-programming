68f136b03025c55944121ab230b3208f78a55dd4
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-10 00:05:03 +0100

    Alternative to HTTPConnection. done

---
+++ Networking/src/com/drronidz/ApacheTest.java
@@ -0,0 +1,48 @@
+package com.drronidz;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 6/9/2021 , 
+    CREATED ON : 11:48 PM
+*/
+
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ApacheTest {
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://example.org/somepage.html");
+        request.addHeader("User-Agent", "Chrome");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(request);
+            System.out.println("response code = " + response.getCode());
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent())
+            );
+            String line;
+
+            while((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- Networking/src/com/drronidz/Main.java
+++ Networking/src/com/drronidz/Main.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         try {
-            URL url = new URL("http://example.org");
+            URL url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=dogs");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent", "Chrome");
@@ -26,6 +26,7 @@ public class Main {
 
             if(responseCode != 200) {
                 System.out.println("Error reading web page");
+                System.out.println(connection.getResponseMessage());
                 return;
             }
             BufferedReader inputReader = new BufferedReader(
